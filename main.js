5d5bfbb132a307417f661e52fbc2d6d521421f4f
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-14 08:54:41 +0000

    Added registeredEarly boolean variable

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 // Generates a random whole number between 0 - 999 
 let raceNumber = Math.floor(Math.random() * 1000);
+// Indicates whether a runner registered early or not
+let registeredEarly = false;
