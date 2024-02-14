d17e632cc9e9a5ea487a0c985f0a756ba75fbb51
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-14 09:05:47 +0000

    Added control flow statement that checks whether the runner is an adult AND registered early

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ let raceNumber = Math.floor(Math.random() * 1000);
 let registeredEarly = false;
 // Variable for the runner’s age
 const runnersAge = 54;
+
+
+// Checks whether the runner is an adult AND registered early
+if (runnersAge >= 18 && registeredEarly) {
+    raceNumber += 1000;
+}
