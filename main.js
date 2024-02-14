34a1caf7650ecf7881e13513652ab67b9cc8c083
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-14 08:57:11 +0000

    Added runnersAge variable

--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 // Indicates whether a runner registered early or not
 let registeredEarly = false;
+// Variable for the runner’s age
+const runnersAge = 54;
